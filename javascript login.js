770d71bb5317a864b25ad3deb6221f6f0aa30262
256-art <126977205+256-art@users.noreply.github.com>
2025-04-24 11:36:19 +0700

    Add files via upload

--- javascript login.js
+++ javascript login.js
@@ -1,19 +1,11 @@
-function login() {
-    var usernameInput = document.getElementById("username").value;
-    var passwordInput = document.getElementById("password").value;
-
-    // Hashing username & password
-    var hashedUsername = CryptoJS.SHA256(usernameInput).toString();
-    var hashedPassword = CryptoJS.SHA256(passwordInput).toString();
-
-    // Hash hasil dari "franx" dan "liga1en"
-    var correctUsernameHash = "3f0f1f3b9c5f8d8e8e934bff457a5408c716db9deaa8a8473bb77f9e27cd8c68";
-    var correctPasswordHash = "c4ca4238a0b923820dcc509a6f75849b";
-
-    if (hashedUsername === correctUsernameHash && hashedPassword === correctPasswordHash) {
-        alert("Login berhasil!");
-        window.location.href = "symbol.html";
-    } else {
-        alert("Username atau password salah!");
+    function login() {
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
+        
+        if (username === "franx" && password === "liga1en") {
+            alert("Login berhasil!");
+            window.location.href = "halaman_berikutnya.html";
+        } else {
+            alert("Username atau password salah!");
+        }
     }
-}
